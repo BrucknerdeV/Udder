f94cd0ed8c4a61dbce29dcf7f58e875910e3675b
Bruckner de Villiers <bruckner@outlook.com>
2020-06-10 12:56:15 +0200

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Bruckner de Villiers on 2020/06/10.
+//  Copyright © 2020 Bruckner de Villiers. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
